1172f5c3e13f3be079a5a61cc85fe58012f7f8c7
xiaoxian521 <1923740402@qq.com>
2021-11-28 23:37:55 +0800

    docs: update

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -9,34 +9,15 @@ module.exports = [
       { text: '布局', link: '/pages/93bd0d/' },
       { text: '路由', link: '/pages/782b6e/' },
       { text: '权限', link: '/pages/7a0a31/' },
-      { text: '后端接口', link: '/pages/9a083a/' },
-      { text: '导航&标签', link: '/pages/3c2345/' },
-      { text: 'SCSS及样式', link: '/pages/d8cf31/' },
-      { text: '构建&部署', link: '/pages/c43f27/' },
       {
         text: '进阶', items: [
-          { text: '组件&指令', link: '/pages/14e8e1/' },
-          { text: '图表', link: '/pages/235933/' },
           { text: '图标', link: '/pages/497c2e/' },
           { text: '主题', link: '/pages/1579c5/' },
           { text: '国际化', link: '/pages/7dd925/' },
         ]
       },
-      {
-        text: '其他', items: [
-          { text: '其他', link: '/pages/others/' },
-        ]
-      }
     ]
   },
-  // {
-  //   text: '辅助', link: '/pages/developguide/',
-  //   items: [
-  //     { text: '前端开发规范', link: '/pages/developguide/' },
-  //     { text: 'Typora图片上传', link: '/pages/picupload/' },
-  //     { text: '遗留问题', link: '/pages/bea9d4/' },
-  //   ]
-  // },
   {
     text: '相关链接', link: '',
     items: [
