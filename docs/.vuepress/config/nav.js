e16940e9c73a593f3d5a4278d89415d48062d60d
huohuoit <534794892@qq.com>
2021-12-03 16:42:40 +0800

    feat: 权限补充，请求接口文档增加

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -9,6 +9,8 @@ module.exports = [
       { text: '布局', link: '/pages/93bd0d/' },
       { text: '路由', link: '/pages/782b6e/' },
       { text: '权限', link: '/pages/7a0a31/' },
+      { text: '请求接口', link: '/pages/9a083a/' },
+
       {
         text: '进阶', items: [
           { text: '图标', link: '/pages/497c2e/' },
