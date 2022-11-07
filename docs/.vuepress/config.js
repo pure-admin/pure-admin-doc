2ec794cd98394c8a55adb41df97a6ad0ead3909b
xiaoxian521 <1923740402@qq.com>
2022-11-07 19:52:45 +0800

    chore: update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -143,8 +143,8 @@ module.exports = {
 
     ['one-click-copy', { // 代码块复制按钮
       copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-      copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
-      duration: 300, // prompt message display time.
+      copyMessage: '已复制', // default is 'Copy successfully and then paste it for use.'
+      duration: 1000, // prompt message display time.
       showInMobile: false // whether to display on the mobile side, default: false.
     }],
     ['demo-block', { // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
