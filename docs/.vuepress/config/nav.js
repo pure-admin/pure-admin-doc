327000e2078213e13d45b5d00f98e65cc8ef3363
xiaoxian521 <1923740402@qq.com>
2023-02-13 18:21:24 +0800

    docs: update

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -40,9 +40,9 @@ module.exports = [
         items: [
           { text: '常见问题', link: '/pages/FAQ/' },
           { text: '非平台问题跟踪记录', link: '/pages/track/' },
+          { text: '反馈支持', link: '/pages/support/' },
           { text: 'git常用命令', link: '/pages/git/' },
           { text: '技术网站推荐', link: '/pages/recommendation/' },
-          { text: '反馈支持', link: '/pages/support/' },
         ]
       },
     ]
