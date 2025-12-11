36423eea044ef7a0d19838cfdf8c350512c90fe7
xiaoxian521 <1923740402@qq.com>
2025-12-11 10:28:59 +0800

    chore: update

--- theme-vdoing/components/Sidebar.vue
+++ theme-vdoing/components/Sidebar.vue
@@ -2,18 +2,18 @@
   <aside class="sidebar">
     <div style="margin-top: 0.8em;margin-left: 1.2em;" v-show="isShow">
       <a class="pure-video" href="https://pure-admin.cn/pages/service/"
-        style="width:86%;background: var(--videoBgColor);padding:10px;border-radius: 6px;font-size: 18px;">
-        <span style="color: #ed5858">高级服务<span style="color: #ed5858;font-size: 16px;">（新功能更新） </span></span>
+        style="background: var(--videoBgColor);padding:8px;border-radius: 6px;margin-bottom: 4px;color: var(--videoTextColor)">
+        <span style="color: #ed5858">高级服务（新功能更新）</span>
       </a>
       <!-- <a class="pure-video" href="https://pure-admin.cn/pages/service/#%E6%9C%80%E6%96%B0%E6%B4%BB%E5%8A%A8"
-        style="background: var(--videoBgColor);padding:8px;border-radius: 6px;margin-bottom: 4px;font-size: 16px;">
-        <span style="color: #ed5858">Js版本、Max版本优惠活动</span>
-      </a> -->
+          style="background: var(--videoBgColor);padding:8px;border-radius: 6px;margin-bottom: 4px;font-size: 16px;">
+          <span style="color: #ed5858">Js版本、Max版本优惠活动</span>
+        </a> -->
       <!-- <a class="pure-video" href="https://pure-admin.cn/pages/service/#最新活动与动态"
-        style="background: var(--videoBgColor);padding:8px;border-radius: 6px;margin-bottom: 4px;font-size: 16px;">
-        <span style="color: #ed5858">平台最新活动与动态 🔔</span>
-      </a> -->
-      <!-- <a class="pure-video" href="https://www.bilibili.com/video/BV1kg411v7QT/"
+          style="background: var(--videoBgColor);padding:8px;border-radius: 6px;margin-bottom: 4px;font-size: 16px;">
+          <span style="color: #ed5858">平台最新活动与动态 🔔</span>
+        </a> -->
+      <a class="pure-video" href="https://www.bilibili.com/video/BV1kg411v7QT/"
         style="background: var(--videoBgColor);padding:8px;border-radius: 6px;margin-bottom: 4px;color: var(--videoTextColor)"
         target="_blank">快速开发教程 📺</a>
       <a class="pure-video" href="https://pure-admin.cn/pages/FAQ/#快速入门-新手必看"
@@ -22,26 +22,26 @@
       </a>
       <a class="pure-video" href="https://pure-admin-utils.netlify.app/"
         style="background: var(--videoBgColor);padding:8px;border-radius: 6px;margin-bottom: 4px;color: var(--videoTextColor)"
-        target="_blank">@pureadmin/utils文档 📕</a> -->
-      <!-- <a class="pure-video" href="https://www.bilibili.com/video/BV1Rx4y1U7Mv/"
+        target="_blank">@pureadmin/utils文档 📕</a>
+      <a class="pure-video" href="https://www.bilibili.com/video/BV1Rx4y1U7Mv/"
         style="background: var(--videoBgColor);padding:8px;border-radius: 6px;margin-bottom: 4px;color: var(--videoTextColor)"
-        target="_blank">平台页面、功能演示 📺</a> -->
-      <!-- <a class="pure-video" href="https://www.bilibili.com/video/BV1Wr421F7oj/"
+        target="_blank">平台页面、功能演示 📺</a>
+      <a class="pure-video" href="https://www.bilibili.com/video/BV1Wr421F7oj/"
         style="background: var(--videoBgColor);padding:8px;border-radius: 6px;color: var(--videoTextColor)"
-        target="_blank">迁移JS版本教程 📺</a> -->
+        target="_blank">迁移JS版本教程 📺</a>
     </div>
 
     <!-- <div class="blogger" v-if="blogger">
-      <img :src="blogger.avatar" />
-      <div class="blogger-info">
-        <h3>{{ blogger.name }}</h3>
+        <img :src="blogger.avatar" />
+        <div class="blogger-info">
+          <h3>{{ blogger.name }}</h3>
 
-        <div class="icons" v-if="blogger.social">
-          <a :href="item.link" :title="item.title" :class="['iconfont', item.iconClass]"
-            v-for="(item, index) in blogger.social.icons" :key="index" target="_blank"></a>
-        </div>
-        <span v-else>{{ blogger.slogan }}</span>
-      </div> -->
+          <div class="icons" v-if="blogger.social">
+            <a :href="item.link" :title="item.title" :class="['iconfont', item.iconClass]"
+              v-for="(item, index) in blogger.social.icons" :key="index" target="_blank"></a>
+          </div>
+          <span v-else>{{ blogger.slogan }}</span>
+        </div> -->
     <!-- </div> -->
 
     <!-- 移动端Nav -->
